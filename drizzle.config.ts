6e2e736ce948c1ddc24eb46a090f7e669abcbc49
Brian Ho <YWHo@users.noreply.github.com>
2024-01-19 00:58:27 +1300

    setup neoDB connection and drizzleORM

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" }); // define process.env
+
+export default {
+  driver: "pg",
+  schema: "./src/lib/db/schema.ts",
+  dbCredentials: { connectionString: process.env.DATABASE_URL! },
+} satisfies Config;
