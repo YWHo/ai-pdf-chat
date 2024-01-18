a274818f1f911eae4bf3b97e7371a35e0cd1ae7a
Brian Ho <YWHo@users.noreply.github.com>
2024-01-18 23:04:37 +1300

    setup authentication with Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
 import { Button } from "@/components/ui/button";
+import { auth, UserButton } from "@clerk/nextjs";
+import { LogInIcon } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
   return (
-    <div>
-      <h1 className="text-red-600">Hello World</h1>
-      <Button>Click Me</Button>
+    // Note: refer to https://hypercolor.dev to get gradient color
+    //       setting on tailwind
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-2 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to
+            instantly answer questions and understand research with AI
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>file upload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started! <LogInIcon className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
